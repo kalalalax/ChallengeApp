bf08b30fabbf18f054e4b916841f51800aa14ebe
kalalala <x3kucala@gmail.com>
2024-03-23 16:39:11 +0100

    Zadanie dzień 6-ty

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,72 +1,45 @@
-﻿int number = 12777;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallangeApp;
 
-Console.WriteLine(number);
-Console.WriteLine("Podaj ilość cyfr w ww. liczbie");
+Employee employee1 = new Employee("Adam" , "Kowalski" , 30);
+Employee employee2 = new Employee("Monika" , "Nowak" , 18);
+Employee employee3 = new Employee("Zuzia" , "Wiśniewska" , 19);
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
-
-
-foreach (char letter in letters)
+List<Employee> employees = new List<Employee>()
 {
-    if(letter == '0')
-     {
-         counter0++;
-     }
-    else if (letter == '1')
+    employee1, employee2 , employee3
+};
+
+employee1.AddScore(1);
+employee1.AddScore(2);
+employee1.AddScore(5);
+employee1.AddScore(5);
+employee1.AddScore(7);
+
+employee2.AddScore(2);
+employee2.AddScore(7);
+employee2.AddScore(8);
+employee2.AddScore(1);
+employee2.AddScore(5);
+
+employee3.AddScore(1);
+employee3.AddScore(3);
+employee3.AddScore(4);
+employee3.AddScore(10);
+employee3.AddScore(6);
+
+
+int maxScore = -1;
+Employee employeeWithMaxScore = null;
+
+foreach(var employee in employees)
+{
+    if (employee.Score > maxScore)
     {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxScore = employee;
     }
 }
-Console.WriteLine("0 = " + counter0);
-Console.WriteLine("1 = " + counter1);
-Console.WriteLine("2 = " + counter2);
-Console.WriteLine("3 = " + counter3);
-Console.WriteLine("4 = " + counter4);
-Console.WriteLine("5 = " + counter5);
-Console.WriteLine("6 = " + counter6);
-Console.WriteLine("7 = " + counter7);
-Console.WriteLine("8 = " + counter8);
-Console.WriteLine("9 = " + counter9);
+
+Console.WriteLine("Employee with the highest score is");
+Console.WriteLine($"{employeeWithMaxScore.Name} {employeeWithMaxScore.Surname} {employeeWithMaxScore.Age} years old");
+Console.WriteLine($"She get {employeeWithMaxScore.Score} points. ");
+Console.WriteLine("Good job.");
